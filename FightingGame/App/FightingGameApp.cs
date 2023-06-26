6a1224a03e8b57637ad0aa24dca120ecfe3b8c18
Lauren Goss <laurenlgoss98@gmail.com>
2023-06-26 14:54:32 -0400

    begin app architecture

---
+++ FightingGame/App/FightingGameApp.cs
@@ -0,0 +1,31 @@
+﻿namespace FightingGame.App;
+
+/// <summary>
+/// Manages fighting game app workflow.
+/// </summary>
+public class FightingGameApp
+{
+    public FightingGameApp()
+    {
+    }
+
+    /// <summary>
+    /// Starts the fighting game application.
+    /// </summary>
+    public void Run()
+    {
+        // Prompt user to build character
+
+
+        // Generate enemy
+
+
+        // Start fight
+
+
+        // Show result
+
+
+        throw new NotImplementedException();
+    }
+}
--- FightingGame/Program.cs
+++ FightingGame/Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FightingGame.App;
 
+try
+{
+    FightingGameApp app = new();
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
